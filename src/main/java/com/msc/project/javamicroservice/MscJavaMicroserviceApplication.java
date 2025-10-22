6cce5c9f14b634accf231c59df5706ddebd1d545
zm24acgRiyan <zm24acg@herts.ac.uk>
2025-10-23 00:51:49 +0100

    Moved Spring Boot app into main CI/CD repo

---
+++ src/main/java/com/msc/project/javamicroservice/MscJavaMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.msc.project.javamicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscJavaMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MscJavaMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msc/project/javamicroservice/MscJavaMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msc.project.javamicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscJavaMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
